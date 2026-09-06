3c23e06708f2ad23aab1ab4a3ffd375e2cc30675
shubhankaraarya <shubhankaraarya@gmail.com>
2026-09-06 14:50:54 +0530

    Update var

--- 02-Vars and Data Types/Variable.java
+++ 02-Vars and Data Types/Variable.java
@@ -3,5 +3,13 @@ public class Variable {
     public static void main(String args[]) {
         int x = 2;
         System.out.println(x+2);
+        System.out.println(x/2);
+        System.out.println(x*3);
+        System.out.println(5+4+"Java"+3+9);
+
+        String name;
+        name = ("Shubhankar");
+
+        System.out.println(name);
     }
 }
