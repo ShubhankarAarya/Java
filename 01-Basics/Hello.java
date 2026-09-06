c3adef6883cff1ecdbb7e28c4c4f417a5faad4d1
shubhankaraarya <shubhankaraarya@gmail.com>
2026-09-06 14:30:02 +0530

    Update Hello Proram

--- 01-Basics/Hello.java
+++ 01-Basics/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Shubhankar!");
+        System.out.println("Hello, Shubhankar K. Aarya!");
     }
 }
